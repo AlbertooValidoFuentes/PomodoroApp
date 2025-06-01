80e341e206a16f24a4f9ec1c8611ca6ecef1a074
Alberto Valido Fuentes <albertovalidofuentes2001@gmail.com>
2025-06-01 21:02:34 +0100

    Initial Commit

---
+++ PomodoroApp/PomodoroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroAppApp.swift
+//  PomodoroApp
+//
+//  Created by Alberto Valido Fuentes on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PomodoroAppTests/PomodoroAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroAppTests.swift
+//  PomodoroAppTests
+//
+//  Created by Alberto Valido Fuentes on 1/6/25.
+//
+
+import Testing
+@testable import PomodoroApp
+
+struct PomodoroAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
